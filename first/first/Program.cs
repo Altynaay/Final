56c2a9e9f19bbadc54fcc3d3387d0a3e0e8960b3
peyote2504 <maratovna362@gmail.com>
2017-05-20 09:27:14 +0600

    first comnit

---
+++ first/first/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace first
+{
+    class Program
+    {
+        static void recursion()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Admin\Documents\Task1");
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name + ":");
+            }
+        } 
+        static void Main(string[] args)
+        {
+            recursion();
+            Console.ReadKey();
+        }
+    }
+}
